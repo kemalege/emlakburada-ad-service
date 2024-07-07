50987c9c0f8e66244fb989d78ce56c87c4567e44
kemalege <cirpanliege@gmail.com>
2024-07-07 19:42:31 +0300

    Create AdConverter

---
+++ src/main/java/com/patika/emlakburadaadservice/converter/AdConverter.java
@@ -0,0 +1,42 @@
+package com.patika.emlakburadaadservice.converter;
+
+import com.patika.emlakburadaadservice.dto.request.AdSaveRequest;
+import com.patika.emlakburadaadservice.dto.response.AdResponse;
+import com.patika.emlakburadaadservice.model.Ad;
+import com.patika.emlakburadaadservice.model.enums.AdStatus;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AdConverter {
+
+    public static Ad convert(AdSaveRequest request, Long customerId) {
+        return Ad.builder()
+                .createDate(LocalDateTime.now())
+                .adStatus(AdStatus.IN_REVIEW)
+                .adCode("ad-code" + request.getCustomerId())
+                .customerId(customerId)
+                .build();
+    }
+
+    public static AdResponse toResponse(Ad ad) {
+        return AdResponse.builder()
+                .adCode(ad.getAdCode())
+                .adStatus(ad.getAdStatus())
+                .id(ad.getId())
+                .createDate(ad.getCreateDate())
+                .build();
+    }
+
+    public static Set<AdResponse> toResponse(List<Ad> ads) {
+        return ads
+                .stream()
+                .map(AdConverter::toResponse)
+                .collect(Collectors.toSet());
+    }
+}
