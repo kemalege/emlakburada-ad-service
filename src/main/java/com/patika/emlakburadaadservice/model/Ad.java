b2d430e849da4f0a4dea5540957232d1a0fdcdd5
kemalege <cirpanliege@gmail.com>
2024-07-08 00:22:25 +0300

    Add Builder

--- src/main/java/com/patika/emlakburadaadservice/model/Ad.java
+++ src/main/java/com/patika/emlakburadaadservice/model/Ad.java
@@ -3,6 +3,7 @@ package com.patika.emlakburadaadservice.model;
 import com.patika.emlakburadaadservice.model.enums.AdStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "orders")
 public class Ad {
 
