00ad17e4b4be47e21032d14370c6c4e469c7ee41
kemalege <cirpanliege@gmail.com>
2024-07-08 02:21:32 +0300

    Create AdController and implement create and getAll controllers

---
+++ src/main/java/com/patika/emlakburadaadservice/controller/AdController.java
@@ -0,0 +1,33 @@
+package com.patika.emlakburadaadservice.controller;
+
+import com.patika.emlakburadaadservice.dto.request.AdSaveRequest;
+import com.patika.emlakburadaadservice.dto.request.AdSearchRequest;
+import com.patika.emlakburadaadservice.dto.response.AdResponse;
+import com.patika.emlakburadaadservice.dto.response.GenericResponse;
+import com.patika.emlakburadaadservice.service.AdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/ads")
+public class AdController {
+
+    private final AdService adService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody AdSaveRequest request){
+        adService.create(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<GenericResponse<Set<AdResponse>>> getAll(@RequestBody AdSearchRequest request) {
+        return new ResponseEntity<>(GenericResponse.success(adService.getAll(request)), HttpStatus.OK);
+    }
+
+}
