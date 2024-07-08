ef58cc9c29f59c8d416d80d888012c0940db344e
kemalege <cirpanliege@gmail.com>
2024-07-08 21:14:40 +0300

    Enable feign client

--- src/main/java/com/patika/emlakburadaadservice/EmlakburadaAdServiceApplication.java
+++ src/main/java/com/patika/emlakburadaadservice/EmlakburadaAdServiceApplication.java
@@ -3,9 +3,11 @@ package com.patika.emlakburadaadservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class EmlakburadaAdServiceApplication {
 
     public static void main(String[] args) {
