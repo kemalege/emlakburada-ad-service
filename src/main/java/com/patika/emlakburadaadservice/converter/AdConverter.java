5b8ae4bd8da49f6b290907c6a34ed8ecbb6cf7c4
kemalege <cirpanliege@gmail.com>
2024-07-11 23:57:05 +0300

    Expanded toResponseWithUser by including user details

--- src/main/java/com/patika/emlakburadaadservice/converter/AdConverter.java
+++ src/main/java/com/patika/emlakburadaadservice/converter/AdConverter.java
@@ -1,5 +1,6 @@
 package com.patika.emlakburadaadservice.converter;
 
+import com.patika.emlakburadaadservice.client.user.dto.response.UserResponse;
 import com.patika.emlakburadaadservice.dto.request.AdSaveRequest;
 import com.patika.emlakburadaadservice.dto.response.AdResponse;
 import com.patika.emlakburadaadservice.model.Ad;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -21,25 +23,59 @@ public class AdConverter {
         String adCode = "ad-" + uuid;
         return Ad.builder()
                 .createDate(LocalDateTime.now())
+                .title(request.getTitle())
+                .price(request.getPrice())
+                .category(request.getCategory())
+                .location(request.getLocation())
+                .adDetails(request.getDetails())
+                .imageUrl(request.getImageUrl())
                 .adStatus(AdStatus.IN_REVIEW)
                 .adCode(adCode)
                 .userId(userId)
                 .build();
     }
 
-    public static AdResponse toResponse(Ad ad) {
+    public static AdResponse toResponseWithUser(Ad ad, UserResponse userResponse) {
         return AdResponse.builder()
+                .title(ad.getTitle())
+                .price(ad.getPrice())
+                .category(ad.getCategory())
+                .location(ad.getLocation())
+                .details(ad.getAdDetails())
+                .imageUrl(ad.getImageUrl())
                 .adCode(ad.getAdCode())
                 .adStatus(ad.getAdStatus())
                 .id(ad.getId())
+                .user(userResponse)
                 .createDate(ad.getCreateDate())
                 .build();
     }
 
+    public static AdResponse toResponse(Ad ad) {
+        return AdResponse.builder()
+                .id(ad.getId())
+                .title(ad.getTitle())
+                .price(ad.getPrice())
+                .category(ad.getCategory())
+                .location(ad.getLocation())
+                .details(ad.getAdDetails())
+                .imageUrl(ad.getImageUrl())
+                .adCode(ad.getAdCode())
+                .adStatus(ad.getAdStatus())
+                .createDate(ad.getCreateDate())
+                .userId(ad.getUserId())
+                .build();
+    }
+
     public static Set<AdResponse> toResponse(List<Ad> ads) {
-        return ads
-                .stream()
+        return ads.stream()
                 .map(AdConverter::toResponse)
+                .collect(Collectors.toSet());
+    }
+
+    public static Set<AdResponse> toResponseWithUser(List<Ad> ads, Map<Long, UserResponse> userResponses) {
+        return ads.stream()
+                .map(ad -> toResponseWithUser(ad, userResponses.get(ad.getUserId())))
                 .collect(Collectors.toSet());
     }
 }
