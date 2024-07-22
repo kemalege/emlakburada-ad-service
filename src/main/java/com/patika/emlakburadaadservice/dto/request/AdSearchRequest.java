f85d0d519423f1a90b13cc522f9dc7df2feef489
kemalege <cirpanliege@gmail.com>
2024-07-22 04:37:10 +0300

    Add adstatus

--- src/main/java/com/patika/emlakburadaadservice/dto/request/AdSearchRequest.java
+++ src/main/java/com/patika/emlakburadaadservice/dto/request/AdSearchRequest.java
@@ -1,5 +1,6 @@
 package com.patika.emlakburadaadservice.dto.request;
 
+import com.patika.emlakburadaadservice.model.enums.AdStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,6 @@ import java.math.BigDecimal;
 public class AdSearchRequest extends BaseSearchRequest {
 
     private Long userId;
+    private AdStatus adStatus;
     private String sort;
 }
