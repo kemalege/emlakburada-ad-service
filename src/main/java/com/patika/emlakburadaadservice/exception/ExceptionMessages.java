e12c4742e7e456ed2ead8962973738a9dc623868
kemalege <cirpanliege@gmail.com>
2024-07-22 04:29:37 +0300

    Add exception message

--- src/main/java/com/patika/emlakburadaadservice/exception/ExceptionMessages.java
+++ src/main/java/com/patika/emlakburadaadservice/exception/ExceptionMessages.java
@@ -6,4 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionMessages {
     public static final String AD_NOT_FOUND = "İlan bulunamadı.";
+    public static final String AD_NOT_RIGHTS = "İlan yayınlama hakkınız bulunmamaktadır.";
 }
